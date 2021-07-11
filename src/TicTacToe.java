112096821739174bf8fac9e2f6f41d27f0ddf558
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-11 17:46:03 +0300

    some initial set up

---
+++ src/TicTacToe.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener{
+
+    TicTacToe() {
+
+    } 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public void firstTurn(){
+
+    }
+    public void 
+    
+    check(){
+
+    }
+
+    public void xWins(int a,int b, int c){
+
+    }
+    
+    
+    public void oWins(int a,int b, int c){
+
+    }
+}
