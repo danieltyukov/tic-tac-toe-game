b34cd1aee52265ec287ac46d1baa9c0fe42fc0be
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-11 17:50:45 +0300

    formatting and declarations

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,32 +3,28 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class TicTacToe implements ActionListener{
+public class TicTacToe implements ActionListener {
 
-    TicTacToe() {
+  Random random = new Random();
+  JFrame frame = new JFrame();
+  JPanel title_panel = new JPanel();
+  JPanel button_panel = new JPanel();
+  JLabel textfield = new JLabel();
+  JButton[] buttons = new JButton[9];
+  boolean player1_turn;
 
-    } 
+  TicTacToe() {
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
-    }
+  @Override
+  public void actionPerformed(ActionEvent e) {}
 
-    public void firstTurn(){
+  public void firstTurn() {}
 
-    }
-    public void 
-    
-    check(){
+  public void check() {}
 
-    }
+  public void xWins(int a, int b, int c) {}
 
-    public void xWins(int a,int b, int c){
-
-    }
-    
-    
-    public void oWins(int a,int b, int c){
-
-    }
+  public void oWins(int a, int b, int c) {}
 }
